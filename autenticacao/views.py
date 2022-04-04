f4b63a8dcab05484038ae3236d90a635134edc43
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-04-04 20:19:54 -0300

    cadastro html, css, view
    cadastro.css adicionado a pasta static, view cadastro configurada, cadastro.html ativado formulário.

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,32 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def cadastro(request):
-    return render(request,'cadastro.html')
+    # Como a requisição/request veio
+    # Requisição pela url
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    # Requisição por formulário
+    elif request.method == "POST":
+        # return HttpResponse('Recebido POST')
+        usuario = request.POST.get('username')
+        senha = request.POST.get('password')
+        confirmar_senha = request.POST.get('confirm-password')
+
+        # Se senhas forem diferentes
+        if not senha == confirmar_senha:
+            return redirect('/cadastro')
+
+        # Se usuário não digitar um nome de usuário ou se não digitar snha. Só espaços não são contados.
+        if len(usuario.strip()) == 0 or len(senha.strip()) == 0:
+            return redirect('/cadastro')
+
+        #user = User.objects.filter(username=username)
+
+        #if user.exists():
+        #    return redirect('/auth/cadastro')
+
+        return HttpResponse('Chegou no fim de POST')
 
 def login(request):
     return render(request,'login.html')

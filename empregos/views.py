c27b352e4a2d02910bd48c5eef64e6f8fc44b444
diego-goncalves-dreossi <dididreo@hotmail.com>
2022-04-06 23:20:22 -0300

    view encontrar_emps atualizada
    Dados do formulário capturados em variáveis

--- empregos/views.py
+++ empregos/views.py
@@ -5,7 +5,18 @@ from .models import Emprego
 def encontrar_emps(request):
     if request.method == 'GET':
         emps = Emprego.objects.filter(reservado = False)
-        print(emps)
+        #print(emps)
+        
+        preco_minimo = request.GET.get('preco_minimo')
+        preco_maximo = request.GET.get('preco_maximo')
+        prazo_minimo = request.GET.get('prazo_minimo')
+        prazo_maximo = request.GET.get('prazo_minimo')
+        categoria = request.GET.get('categoria')
+
+        # Se um dos filtros foi usado
+        if preco_minimo or preco_maximo or prazo_minimo or prazo_maximo or categoria:
+            pass
+
+
         return render(request, 'encontrar_emps.html',{'emps':emps})
-    if request.method == 'POST':
-        preco_minimo = request.POST.get('preco_minimo')
+        return HttpResponse(preco_minimo)
